79f6f4c99169b973d1b6c3b6964aec7ff66be668
Marc Mills <mdmills1990@gmail.com>
2018-07-31 20:44:02 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array,index,value = "X")
+def move(board,index,value = "X")
   board[index] = value
-  puts display_board
 end
